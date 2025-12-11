0e02d254e2f421d3e8460d5777e3e26596164be4
Daniel Sebastian Vargas Gutierrez <danielsvg27@gmail.com>
2025-12-11 00:25:52 -0500

    Correccion Repositorio

--- sc/main/java/com/alpina/sgdv/Service/notificationService.java
+++
@@ -1,9 +0,0 @@
-package com.alpina.sgdv.service;
-import org.springframework.stereotype.Service;
-@Service
-public class NotificationService {
-  public void notifyCustomer(String email, String subject, String body){
-    // Mock: imprime en logs → simula “Notificaciones automáticas”
-    System.out.printf("[EMAIL] to=%s | %s | %s%n", email, subject, body);
-  }
-}
